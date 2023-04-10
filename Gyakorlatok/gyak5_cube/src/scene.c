bfc125766b0beca8d305742c1f9458dddc855617
Dávid Sikora <90835045+Redd-15@users.noreply.github.com>
2023-04-10 15:29:07 +0200

    Upload gyak_5

---
+++ Gyakorlatok/gyak5_cube/src/scene.c
@@ -0,0 +1,130 @@
+#include "scene.h"
+
+#include <obj/load.h>
+#include <obj/draw.h>
+
+void init_scene(Scene* scene)
+{
+    load_model(&(scene->cube), "assets/models/cube.obj");
+    load_model(&(scene->model), "assets/models/cat.obj");
+    scene->texture_id = load_texture("assets/textures/cube.png");
+
+    scene->model_x=0.0;
+    scene->model_y=2.0;
+    scene->model_x_speed=0.0;
+    scene->model_y_speed=0.0;
+
+    glBindTexture(GL_TEXTURE_2D, scene->texture_id);
+
+    scene->material.ambient.red = 0.0;
+    scene->material.ambient.green = 0.0;
+    scene->material.ambient.blue = 0.0;
+
+    scene->material.diffuse.red = 1.0;
+    scene->material.diffuse.green = 1.0;
+    scene->material.diffuse.blue = 0.0;
+
+    scene->material.specular.red = 0.0;
+    scene->material.specular.green = 0.0;
+    scene->material.specular.blue = 0.0;
+
+    scene->material.shininess = 0.0;
+}
+
+void set_lighting()
+{
+    float ambient_light[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+    float diffuse_light[] = { 1.0f, 1.0f, 1.0, 1.0f };
+    float specular_light[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+    float position[] = { 0.0f, 0.0f, 10.0f, 1.0f };
+
+    glLightfv(GL_LIGHT0, GL_AMBIENT, ambient_light);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse_light);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, specular_light);
+    glLightfv(GL_LIGHT0, GL_POSITION, position);
+}
+
+void set_material(const Material* material)
+{
+    float ambient_material_color[] = {
+        material->ambient.red,
+        material->ambient.green,
+        material->ambient.blue
+    };
+
+    float diffuse_material_color[] = {
+        material->diffuse.red,
+        material->diffuse.green,
+        material->diffuse.blue
+    };
+
+    float specular_material_color[] = {
+        material->specular.red,
+        material->specular.green,
+        material->specular.blue
+    };
+
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient_material_color);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse_material_color);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular_material_color);
+
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, &(material->shininess));
+}
+
+void update_scene(Scene* scene, double time)
+{
+
+scene->model_x += time*scene->model_x_speed; 
+scene->model_y += time*scene->model_y_speed; 
+
+}
+
+void render_scene(const Scene* scene)
+{
+    set_material(&(scene->material));
+    set_lighting();
+    draw_origin();
+
+    draw_model(&(scene->cube));
+
+    glTranslatef(0.0, 2.0, 0.0);
+    glRotatef(90.0, 1.0 ,0.0 ,0.0);
+    draw_model(&(scene->model));
+
+    glRotatef(90.0, -1.0, 0.0, 0.0);
+    glTranslatef(2.0, 0.0, 0.0);
+    draw_model(&(scene->cube));
+
+    glRotatef(90.0, 1.0 ,0.0 ,0.0);
+    glTranslatef((scene->model_x), 0.0 ,(scene->model_y));
+    draw_model(&(scene->model));
+}
+
+void set_model_x_speed(Scene* scene, double speed){
+
+    scene->model_x_speed = speed;
+}
+
+void set_model_y_speed(Scene* scene, double speed){
+
+    scene->model_y_speed = speed;
+}
+
+void draw_origin()
+{
+    glBegin(GL_LINES);
+
+    glColor3f(1, 0, 0);
+    glVertex3f(0, 0, 0);
+    glVertex3f(1, 0, 0);
+
+    glColor3f(0, 1, 0);
+    glVertex3f(0, 0, 0);
+    glVertex3f(0, 1, 0);
+
+    glColor3f(0, 0, 1);
+    glVertex3f(0, 0, 0);
+    glVertex3f(0, 0, 1);
+
+    glEnd();
+}
